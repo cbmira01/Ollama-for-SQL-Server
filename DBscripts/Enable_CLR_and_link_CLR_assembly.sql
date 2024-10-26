872799520dc71fdefb4a75dccef191ca2c112d44
Miracle, Calvin B. <calvin.miracle@jefferson.kyschools.us>
2024-10-25 21:09:17 -0400

    SQL code to deploy SQL-CLR assembly and to define functions

--- DBscripts/Enable_CLR_and_link_CLR_assembly.sql
+++ DBscripts/Enable_CLR_and_link_CLR_assembly.sql
@@ -4,8 +4,8 @@ sp_configure 'clr enabled', 1;
 RECONFIGURE;
 GO
 
-CREATE ASSEMBLY ApiCommandLineExecutor
-FROM 'C:\path\to\CommandExecutor.dll'
+CREATE ASSEMBLY SqlClrApiExecutor
+FROM 'C:\Users\cmirac2\Source\PrivateRepos\ApiCommandSqlExecutor\Src\SqlClrApiExecutor\bin\Release\SqlClrApiExecutor.dll'
 WITH PERMISSION_SET = EXTERNAL_ACCESS;
 GO
 
@@ -13,7 +13,7 @@ CREATE FUNCTION ExecuteApiCommand (
     @apiUrl NVARCHAR(MAX), 
     @requestBody NVARCHAR(MAX))
 RETURNS NVARCHAR(MAX)
-AS EXTERNAL NAME ApiCommandLineExecutor.CommandExecutor.ExecuteApiCommand;
+AS EXTERNAL NAME SqlClrApiExecutor.CommandExecutor.ExecuteApiCommand;
 GO
 
 -- Create the CompletePrompt function
@@ -22,7 +22,7 @@ CREATE FUNCTION dbo.CompletePrompt(
   @ask NVARCHAR(MAX), 
   @body NVARCHAR(MAX))
 RETURNS NVARCHAR(MAX)
-AS EXTERNAL NAME ApiCommandLineExecutor.CommandExecutor.CompletePrompt;
+AS EXTERNAL NAME SqlClrApiExecutor.CommandExecutor.CompletePrompt;
 GO
 
 -- Create the CompleteMultiplePrompts function
@@ -32,6 +32,27 @@ CREATE FUNCTION dbo.CompleteMultiplePrompts(
   @body NVARCHAR(MAX), 
   @numCompletions INT)
 RETURNS TABLE (Completion NVARCHAR(MAX))
-AS EXTERNAL NAME ApiCommandLineExecutor.CommandExecutor.CompleteMultiplePrompts;
+AS EXTERNAL NAME SqlClrApiExecutor.CommandExecutor.CompleteMultiplePrompts;
 GO
 
+-- Check if CLR is enabled
+SELECT 
+    name, 
+    value_in_use 
+FROM sys.configurations 
+WHERE name = 'clr enabled';
+GO
+
+-- List all CLR functions and their associated assemblies
+SELECT 
+    asm.name AS AssemblyName,
+    asm.permission_set_desc AS AssemblyPermissionSet,
+    obj.name AS FunctionName,
+    obj.type_desc AS ObjectType,
+    mod.assembly_class AS AssemblyClass,
+    mod.assembly_method AS AssemblyMethod
+FROM sys.assembly_modules mod
+JOIN sys.objects obj ON mod.object_id = obj.object_id
+JOIN sys.assemblies asm ON mod.assembly_id = asm.assembly_id
+WHERE obj.type IN ('FN', 'TF', 'IF'); -- FN = Scalar function, TF = Table-valued function, IF = Inline function
+GO
