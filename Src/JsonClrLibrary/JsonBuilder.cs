eacd565d001462edbb985007ed0daa900a0320e2
Miracle, Calvin B. <calvin.miracle@jefferson.kyschools.us>
2024-10-30 13:46:54 -0400

    Define new JsonBuilder helper class

---
+++ Src/JsonClrLibrary/JsonBuilder.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+public static class JsonBuilder
+{
+    public static KeyValuePair<string, object> CreateField(string key, object value)
+    {
+        return new KeyValuePair<string, object>(key, value);
+    }
+
+    public static KeyValuePair<string, object> CreateObject(string key, params KeyValuePair<string, object>[] fields)
+    {
+        return new KeyValuePair<string, object>(key, new List<KeyValuePair<string, object>>(fields));
+    }
+
+    public static KeyValuePair<string, object> CreateArray(string key, params object[] items)
+    {
+        return new KeyValuePair<string, object>(key, new List<object>(items));
+    }
+}
