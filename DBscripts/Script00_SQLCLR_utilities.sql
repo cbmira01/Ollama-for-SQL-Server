f33bbf94a44dbec652175a5002f7468265da9628
Miracle, Calvin B. <calvin.miracle@jefferson.kyschools.us>
2024-11-08 08:04:24 -0500

    Dump assemblies and functions

--- DBscripts/Script00_SQLCLR_utilities.sql
+++ DBscripts/Script00_SQLCLR_utilities.sql
@@ -1,1 +1,17 @@
 ﻿
+-- List all user-defined assemblies and all CLR functions
+SELECT * FROM sys.assemblies WHERE is_user_defined = 1;
+GO
+
+SELECT 
+    asm.name AS AssemblyName,
+    asm.permission_set_desc AS AssemblyPermissionSet,
+    obj.name AS FunctionName,
+    obj.type_desc AS ObjectType,
+    mod.assembly_class AS AssemblyClass,
+    mod.assembly_method AS AssemblyMethod
+FROM sys.assembly_modules mod
+JOIN sys.objects obj ON mod.object_id = obj.object_id
+JOIN sys.assemblies asm ON mod.assembly_id = asm.assembly_id
+--WHERE obj.type IN ('FN', 'TF', 'IF'); -- FN = Scalar function, TF = Table-valued function, IF = Inline function
+GO
