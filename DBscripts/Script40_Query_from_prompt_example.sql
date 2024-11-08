5c9c4d3e0cf43af55d7dec80e8f5d2bad9babd52
Miracle, Calvin B. <calvin.miracle@jefferson.kyschools.us>
2024-11-08 07:56:01 -0500

    Add new utility script

--- DBscripts/Script40_Query_from_prompt_example.sql
+++ DBscripts/Script40_Query_from_prompt_example.sql
@@ -1,24 +1,24 @@
 
 
-USE [Test];
-GO
+-- SQL coding for an eventual 'QueryFromPrompt' feature
 
+--USE [Test];
+--GO
 
-CREATE TABLE QueryPromptLog (
-    LogID INT IDENTITY(1,1) PRIMARY KEY,
-    Prompt NVARCHAR(MAX) NULL,
-    ProposedQuery NVARCHAR(MAX) NULL,
-    ErrorNumber NVARCHAR(10) NULL,
-    ErrorMessage NVARCHAR(100) NULL,
-    ErrorLine NVARCHAR(10) NULL,
-    [Timestamp] DATETIME NOT NULL DEFAULT(getdate())
-);
-GO
+--CREATE TABLE QueryPromptLog (
+--    LogID INT IDENTITY(1,1) PRIMARY KEY,
+--    Prompt NVARCHAR(MAX) NULL,
+--    ProposedQuery NVARCHAR(MAX) NULL,
+--    ErrorNumber NVARCHAR(10) NULL,
+--    ErrorMessage NVARCHAR(100) NULL,
+--    ErrorLine NVARCHAR(10) NULL,
+--    [Timestamp] DATETIME NOT NULL DEFAULT(getdate())
+--);
+--GO
 
+--DECLARE @modelName NVARCHAR(MAX) = 'llama3.2';
+--DECLARE @prompt NVARCHAR(MAX) = 'Find all entries in support_emails where sentiment is glad.';
 
-DECLARE @modelName NVARCHAR(MAX) = 'llama3.2';
-DECLARE @prompt NVARCHAR(MAX) = 'Find all entries in support_emails where sentiment is glad.';
-
-SELECT dbo.QueryFromPrompt(@modelName, @prompt);
-GO
+--SELECT dbo.QueryFromPrompt(@modelName, @prompt);
+--GO
 
