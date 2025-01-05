1df3bb87ef4951c42f756c018099e7823928192e
Miracle, Calvin B. <calvin.miracle@jefferson.kyschools.us>
2025-01-04 22:36:39 -0500

    Two ways to load image dfata

--- DBscripts/Script22_Load_image_data.sql
+++ DBscripts/Script22_Load_image_data.sql
@@ -1,21 +1,18 @@
 
 /**
 
-    This script populates the Images table for image classification studies.
+    This script recreates  the Images table for image classification studies.
 
-    Use Script10 to ensure that a TEST database is available on your database 
-        server with permissions for CLR integration.
+    To populate the Images table with test data, you can either:
+    - Run console program 'LoadImageFiles'
+    - Uncomment SQL code below to use 'xp_cmdshell' (requires copying image files to an accessible directory)
+
+    Use Script10 to ensure that a TEST database is available on your database server.
 
     Use Script20 to populate the TEST database with demonstration data.
 
-    Make sure the @RepositoryPath symbol is set to your local repository location.
-
     Tests of image classification can be found in Script70.
 **/
-
--- DECLARE @RepositoryPath NVARCHAR(200) = 'C:\Users\cmirac2\Source\PrivateRepos\Ollama-for-SQL-Server';
-DECLARE @RepositoryPath NVARCHAR(200) = 'C:\Temp\';
-DECLARE @ImagesPath NVARCHAR(100) = 'Images\';
 
 USE [TEST];
 
@@ -31,6 +28,24 @@ CREATE TABLE Images (
     ImageData VARBINARY(MAX) NOT NULL
 );
 
+
+/********************************************************************************
+-- The code commented here is an alternative way to load image data,
+-- other than by using the 'LoadImageFiles' console program.
+
+-- To use 'xp_cmdshell' to populate image data, follow these steps:
+
+--   1. Uncomment this block of SQL code.
+
+--   2. Make sure the @ImagesPath symbol is set to a directory accessible by 'xp_cmdshell'.
+--      The C:\Temp\Images folder should work quite well.
+
+--   3. Copy JPEG files from <repository_root>\Images directory into C:\Temp\Images\
+
+--   4. Run the entire script to drop/create the table and bulk-insert the image files.
+
+DECLARE @ImagesPath NVARCHAR(200) = 'C:\Temp\Images\';
+
 ---------------------------------------------------------------------------------
 -- Enable xp_cmdshell for image loading
 ---------------------------------------------------------------------------------
@@ -45,13 +60,12 @@ EXEC sp_configure 'xp_cmdshell';
 ---------------------------------------------------------------------------------
 -- Bulk Insert Images from Files
 ---------------------------------------------------------------------------------
-DECLARE @FolderPath NVARCHAR(255) = @RepositoryPath + '\' + @ImagesPath ;
 DECLARE @FileName NVARCHAR(255);
 DECLARE @FullFilePath NVARCHAR(255);
 
 CREATE TABLE #FileList (FileName NVARCHAR(255));
 
-DECLARE @Command NVARCHAR(255) = 'dir   ' + @FolderPath + '   /b';
+DECLARE @Command NVARCHAR(255) = 'dir   ' + @ImagesPath + '   /b';
 INSERT INTO #FileList (FileName)
 EXEC xp_cmdshell @Command;
 
@@ -64,7 +78,7 @@ FETCH NEXT FROM FileCursor INTO @FileName;
 
 WHILE @@FETCH_STATUS = 0
 BEGIN
-    SET @FullFilePath = @FolderPath + @FileName;
+    SET @FullFilePath = @ImagesPath + @FileName;
 
     BEGIN TRY
         -- Use dynamic SQL for OPENROWSET
@@ -101,3 +115,5 @@ RECONFIGURE;
 
 EXEC sp_configure 'show advanced options', 0;
 RECONFIGURE;
+
+********************************************************************************/
---
+++ LoadImageFiles/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.IO;
+
+/***
+ * 
+ * This program will load image files into the TEST database Images table.
+ * 
+ * Only unique filenames are loaded into the table.
+ * 
+ * Script22 contains an alternative way to load image files.
+ * 
+ ***/
+
+namespace LoadImageFiles
+{
+    class Program
+    {
+        private const string ConnectionString = "Server=localhost;Database=TEST;Integrated Security=SSPI;";
+
+        static void Main(string[] args)
+        {
+            string repoRoot = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\.."));
+            string imagesPath = Path.Combine(repoRoot, "Images");
+
+            try
+            {
+                if (!Directory.Exists(imagesPath))
+                {
+                    Console.WriteLine($"Directory not found: {imagesPath}");
+                    return;
+                }
+
+                string[] imageFiles = Directory.GetFiles(imagesPath, "*.jpg", SearchOption.TopDirectoryOnly);
+
+                if (imageFiles.Length == 0)
+                {
+                    Console.WriteLine("No JPEG files found in the directory.");
+                    return;
+                }
+
+                foreach (string filePath in imageFiles)
+                {
+                    try
+                    {
+                        string fileName = Path.GetFileName(filePath);
+                        byte[] fileData = File.ReadAllBytes(filePath);
+
+                        InsertImage(fileName, fileData);
+                        Console.WriteLine($"Successfully inserted: {fileName}");
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Failed to process file {filePath}: {ex.Message}");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+        }
+
+        private static void InsertImage(string fileName, byte[] fileData)
+        {
+            // Enforce unique filenames
+            string query = @"
+                IF NOT EXISTS (SELECT 1 FROM Images WHERE FileName = @FileName)
+                BEGIN
+                    INSERT INTO Images (FileName, ImageData) VALUES (@FileName, @ImageData)
+                END";
+
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@FileName", fileName);
+                    command.Parameters.AddWithValue("@ImageData", fileData);
+
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
